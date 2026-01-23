a454ed695196633d0b00b646f15e07f9ffcfab42
Jack Allred <info@sguildswim.com>
2026-01-23 04:20:05 -0600

    changed prisma import in lib

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from ".prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 import { Pool } from "pg"
 
