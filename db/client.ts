45922a67d3b893cf3dfbb56b7b0d51c81b4f1587
Jack Allred <info@sguildswim.com>
2026-01-23 04:04:34 -0600

    fix: use shared prisma client in db client

--- db/client.ts
+++ db/client.ts
@@ -1,5 +1,3 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { prisma } from "@/lib/prisma"
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
-export const prisma = new PrismaClient({ adapter })
+export { prisma }
